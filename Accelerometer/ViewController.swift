aa62c78ac50e46d397438a34534e277ab28e1708
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-08-21 20:16:48 +0900

    Initial Commit

---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by 春田実利 on 2022/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
